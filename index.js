fb8a9ea3f735b6954bd4ab8e80d492d0f6538d01
billymwangi <billy.mwangi@student.moringaschool.com>
2022-06-11 08:25:48 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// let sum=0;
+// for (let n of batteryBatches)
+// sum +=n;
+// console.log(sum)
+
+const totalBatteries = batteryBatches.reduce(
+    (accumulator, currentValue) => accumulator + currentValue);
+console.log(totalBatteries)
